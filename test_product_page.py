9c20e33ae9502539b148e3f2ad6644d722366ae4
ne_andrey <sorokinac@gmail.com>
2022-03-22 10:31:45 +0500

    add_pytest_and_labels_test_example_test

--- test_product_page.py
+++ test_product_page.py
@@ -16,7 +16,8 @@ from .pages.login_page import LoginPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def test_guest_can_add_product_to_basket(self, browser, link):
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_button_basket()
@@ -25,7 +26,7 @@ def test_guest_can_add_product_to_basket(self, browser, link):
     page.name_items_is_basket_conside()
     page.price_items_is_basket_conside()
 
-def test_guest_cant_see_success_message(self, browser):
+def test_guest_cant_see_success_message(browser):
     link = "http://selenium0py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
@@ -53,12 +54,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_form_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_form_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
@@ -86,6 +89,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
